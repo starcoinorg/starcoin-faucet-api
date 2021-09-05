7039efd1e39b3147527cc7f348b6883043160165
wytheme <wytheme.go@gmail.com>
2021-09-05 20:51:39 +0800

    add CORS

--- app/main.py
+++ app/main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.core.config import settings
 from app.db.redis import redis_cache
 from app.api import faucet
 
 app = FastAPI(title=settings.SERVER_NAME,
               openapi_url=settings.OPENAPI_URL)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 #
 app.include_router(faucet.router, prefix='')
 #
