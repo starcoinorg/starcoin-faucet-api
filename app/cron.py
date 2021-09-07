9eaa697b693c7517822bf81c0151252661fca913
wk3368 <wk3368@163.com>
2021-09-08 07:25:41 +0800

    add search tag: StarcoinSTC

--- app/cron.py
+++ app/cron.py
@@ -26,19 +26,22 @@ def scrape_twitter(id, url, created_at, db: Session, faucet):
     until = created_at.date() + timedelta(days=1)
     c = twint.Config()
     c.Username = username
-    c.Debug=True
-    #c.Limit=50
-    c.Store_csv = True 
-    c.Output = filename 
+    c.Debug = True
+    # c.Limit=50
+    c.Store_csv = True
+    c.Output = filename
     c.Since = since.strftime("%Y-%m-%d")
     c.Until = until.strftime("%Y-%m-%d")
+    c.Search = "StarcoinSTC"
     twint.run.Search(c)
 
-    logger.info(f'scrape_twitter {since.strftime("%Y-%m-%d")} {until.strftime("%Y-%m-%d")}')
+    logger.info(
+        f'scrape_twitter {since.strftime("%Y-%m-%d")} {until.strftime("%Y-%m-%d")}')
 
     # file
     address = get_address_from_csv(url, filename)
-    logger.info('id since address : {} {} {}'.format(id, since.strftime("%Y-%m-%d"), address))
+    logger.info('id since address : {} {} {}'.format(
+        id, since.strftime("%Y-%m-%d"), address))
 
     # db
     #from app.db.session import SessionLocal
@@ -52,24 +55,30 @@ def scrape_twitter(id, url, created_at, db: Session, faucet):
         return False, id, "already tranfer"
 
     if not address:
-        faucet_crud.faucet.update(db, db_obj=faucet,obj_in={"status": FaucetStatus.fail.value})
+        faucet_crud.faucet.update(db, db_obj=faucet, obj_in={
+                                  "status": FaucetStatus.fail.value})
         return False, id, "none address found"
 
-    count = faucet_crud.faucet.get_day_count_by_address(db=db, network=faucet.network, address=address, since=created_at)
+    count = faucet_crud.faucet.get_day_count_by_address(
+        db=db, network=faucet.network, address=address, since=created_at)
     logger.info(f'address count:{count} {(created_at)} {faucet.platform}')
     if count > 0:
-        faucet_crud.faucet.update(db, db_obj=faucet,obj_in={"status": FaucetStatus.coin_already_transfered.value})
+        faucet_crud.faucet.update(db, db_obj=faucet, obj_in={
+                                  "status": FaucetStatus.coin_already_transfered.value})
         return False, id, "address already transfer"
 
     # coin transfer
-    try:    
+    try:
         txn = p2p_tranfer(faucet.network, address, faucet.amount)
         logger.info(f'id since address : {txn}')
-        logger.info({"address": address, "status": FaucetStatus.success.value, "transfered_txn": txn, "transafered_at": datetime.utcnow() })
-        faucet_crud.faucet.update(db, db_obj=faucet,obj_in={"address": address, "status": FaucetStatus.success.value, "transfered_txn": txn, "transfered_at": datetime.utcnow() })
+        logger.info({"address": address, "status": FaucetStatus.success.value,
+                    "transfered_txn": txn, "transafered_at": datetime.utcnow()})
+        faucet_crud.faucet.update(db, db_obj=faucet, obj_in={
+                                  "address": address, "status": FaucetStatus.success.value, "transfered_txn": txn, "transfered_at": datetime.utcnow()})
     except Exception as exc:
         logger.exception(f"{faucet.id} transfer coin error")
-        faucet_crud.faucet.update(db, db_obj=faucet,obj_in={"status": FaucetStatus.coin_fail.value})
+        faucet_crud.faucet.update(db, db_obj=faucet, obj_in={
+                                  "status": FaucetStatus.coin_fail.value})
         return False, id, "transfer coin error"
 
     # gc
@@ -79,7 +88,7 @@ def scrape_twitter(id, url, created_at, db: Session, faucet):
     return True, id, "success"
 
 
-def p2p_tranfer(network ,address, amount):
+def p2p_tranfer(network, address, amount):
     if not address or not amount:
         raise Exception('wrong args')
 
@@ -98,13 +107,13 @@ def p2p_tranfer(network ,address, amount):
     sender_private_key = cnf.value['senderPrivateKey']
 
     # NanoSTC (1 STC = 1000000000 NanoSTC)
-    # 
+    #
     # amount = amount * (1000000000)
     # reciever_address = address
     # debug
     # !!! debug
     if settings.DEBUG:
-        amount = 1024 # 
+        amount = 1024
         reciever_address = "0x4c7afc223df1d47072194dcefe26a445"
     else:
         amount = amount * 1000000000
@@ -135,7 +144,8 @@ def get_address_from_csv(url, filename):
 
             # !!! tricky, only match tweet path ( twint api does not have a query)
             if normalise_url_without_query(url) != normalise_url_without_query(tweet_url):
-                logger.info(f"url doesn't match {normalise_url_without_query(url)} {normalise_url_without_query(tweet_url)}")
+                logger.info(
+                    f"url doesn't match {normalise_url_without_query(url)} {normalise_url_without_query(tweet_url)}")
                 continue
 
             address = get_address(tweet)
@@ -143,15 +153,15 @@ def get_address_from_csv(url, filename):
             break
 
     return address
-    
+
+
 if __name__ == "__main__":
     with SessionLocal() as db:
-        faucet = faucet_crud.faucet.get_recent_one_by_status(db, status=FaucetStatus.init.value)
+        faucet = faucet_crud.faucet.get_recent_one_by_status(
+            db, status=FaucetStatus.init.value)
         if faucet is None:
             logger.info('no record found')
         else:
-            t = scrape_twitter(faucet.id, faucet.url, faucet.created_at, db, faucet)
+            t = scrape_twitter(faucet.id, faucet.url,
+                               faucet.created_at, db, faucet)
             logger.info(t)
-
-
-
